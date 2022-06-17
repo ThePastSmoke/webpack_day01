3a5e7153b95ce991321a10395f3777691472b8ba
Gzppp <2213041225@qq.com>
2022-06-17 20:59:24 +0800

    '完成了webpack初始化'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 import { tab } from './tab'
-import { marquee } from './tab'
+import { marquee } from './marquee'
 tab()
 marquee()
+
+// 引入css
+import "./style/index.css"
+import "./style/index.less"
+
+// 引入图片
+import gifSrc from "./assets/1.gif"
+import gpngSrc from "./assets/logo_small.png"
+const gif = document.createElement('img')
+const png = document.createElement('img')
+gif.src = gifSrc
+png.src = gpngSrc
+document.body.appendChild(gif)
+document.body.appendChild(png)
+
+// 引入字体图标文件
+import './assets/fonts/iconfont.css'
+
+
+const fn = () => {
+    console.log('你好');
+}
+console.log(fn);
+
+import app from "./app.vue"
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const { join } = require('path')
 // 引入自动生成html的插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const { VueLoaderPlugin } = require('vue-loader')
 module.exports = {
+    mode: "development", // 开发阶段 简易打包，打包速度快 
     entry: './src/main.js',// 入口文件
     output: {
         path: join(__dirname, 'lib'),//出口路径
@@ -16,7 +19,76 @@ module.exports = {
             // 生成后html的文件名称
             filename: 'index.html'
         }),
+        // 请确保引入这个插件！
+        new VueLoaderPlugin()
 
-    ]
+    ],
+    devServer: {
+        // 设置端口号
+        port: 30000,
+        // 自动打开
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // use数组里从右向左运行
+                // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+                // 再用 style-loader 将样式, 把css插入到dom中
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/i,
+                type: 'asset',
+                generator: {
+                    filename: 'images/[hash:6][ext]'
+                },
+                // 自定义最大打包base64图片大小单位字节
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 2 * 1024
+                    }
+                }
+            },
+            {
+                // 处理字体图标解析
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[hash:6][ext]"
+                }
+            },
+            {
+                test: /\.js$/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ],
+
+    },
 
 }
+
+// module.exports = {
+//     module: {
+//         rules: [
+//             // ... 其它规则
+//             {
+//                 test: /\.vue$/,
+//                 loader: 'vue-loader'
+//             }
+//         ]
+//     },
+//     plugins: [
+//         // 请确保引入这个插件！
+//         new VueLoaderPlugin()
+//     ]
+// }
